4c367dd4037cd429dfd3f77fdf08988959be9174
爱新觉罗宇 <yy08183036@163.com>
2019-06-11 20:37:22 +0800

    tianjia

---
+++ src/cn/icast/git/First.java
@@ -0,0 +1,11 @@
+package cn.icast.git;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
